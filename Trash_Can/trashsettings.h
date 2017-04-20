f4e70cd53d78d072f7c222d76a6c0dc29a29570a
Jesper F. Hansen <Jesp.F.Hansen@gmail.com>
2017-04-20 13:23:12 +0200

    Andrej fckd up, asked me to push

--- Trash_Can/trashsettings.h
+++ Trash_Can/trashsettings.h
@@ -14,6 +14,7 @@
 #define TEMPERATUREMAX_DEFAULT 50
 #define HUMIDITYMIN_DEFAULT 20
 #define HUMIDITYMAX_DEFAULT 80
+#define ISLOCKENABLED_DEFAULT 0
 
 #define LANGUAGE_LOWER_BOUNDARY 0
 #define LANGUAGE_UPPER_BOUNDARY 1
@@ -31,6 +32,9 @@
 #define HUMIDITYMIN_UPPER_BOUNDARY 80
 #define HUMIDITYMAX_LOWER_BOUNDARY 20
 #define HUMIDITYMAX_UPPER_BOUNDARY 80
+#define ISLOCKENABLED_LOWER_BOUNDARY 0
+#define ISLOCKENABLED_UPPER_BOUNDARY 1
+
 
 class trashSettings
 {
@@ -40,7 +44,7 @@ public:
     void setDefault();
 
     int language, isProximityEnabled, openingSpeed, detectionRange,
-    temperatureMin, temperatureMax, humidityMin, humidityMax;
+    temperatureMin, temperatureMax, humidityMin, humidityMax, isLockEnabled;
     bool isSettingsOutOfBoundaries;
 
 private:
